4d0d0e8cf76bc193430dfacd3595e5a6b7f38d37
Jon <126274870+jonmc89@users.noreply.github.com>
2025-03-02 12:48:16 +0000

    Sign up form added
    Next steps is confirm styling and password matching functionality and requirements.

--- scripts.js
+++ scripts.js
@@ -1,5 +1,11 @@
-
+/* Flips sign in card */
 document.getElementById('signUp').addEventListener('click', function(event) {
+    event.preventDefault();
+    document.getElementById('welcomeCardInner').classList.toggle('rotate');
+});
+
+/* Unflips sign up card */
+document.getElementById('signUpSubmit').addEventListener('click', function(event) {
     event.preventDefault();
     document.getElementById('welcomeCardInner').classList.toggle('rotate');
 });
